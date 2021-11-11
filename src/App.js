66095fb14088cd546616287b16ae63502d892336
Seema Lathwal <seema.krishan@neosoftmail.com>
2021-11-11 12:32:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hloo </h1>
+      <button>Normal button</button>
+      <Button>Bootstrap Button </Button>
+    </div>
+  );
+}
+
+export default App;
